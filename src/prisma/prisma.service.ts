7bc4c3c1f0b1764f4de3ab26a06d33fc25992a67
Rizqiansyah Ramadhan <rizzthenotable@gmail.com>
2026-06-29 16:09:40 +0700

    feat: add Prisma service and module with Postgres adapter

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,21 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor(config: ConfigService) {
+    super({
+      adapter: new PrismaPg({ connectionString: config.get<string>('databaseUrl') }),
+    });
+  }
+
+  async onModuleInit(): Promise<void> {
+    await this.$connect();
+  }
+
+  async onModuleDestroy(): Promise<void> {
+    await this.$disconnect();
+  }
+}
